bfc88f714cd4a771f99c9ade9201801c64752dbb
Erinson Romero Terry <eromerte@emeal.nttdata.com>
2022-12-15 23:14:59 -0500

    se realizo una tabla dinamica, con un ImageView y label [

--- Mi Cartelera/InicioTops/InicioTopsViewController.swift
+++ Mi Cartelera/InicioTops/InicioTopsViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class InicioTopsViewController: UIViewController {
+    @IBOutlet weak var InicioTopsCollection: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        InicioTopsCollection.delegate = self
+        InicioTopsCollection.dataSource = self
+        title = "Series Populares"
     }
 
 
+}
+
+
+extension InicioTopsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InicioTopsCollectionViewCell", for: indexPath) as! InicioTopsCollectionViewCell
+        return cell
+    }
+    
+    
 }
 
