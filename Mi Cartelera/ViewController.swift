a9a1a61be788f17dcefd764148c9e2a597f03216
Erinson Romero Terry <eromerte@emeal.nttdata.com>
2022-12-15 22:20:55 -0500

    creo el archivo

---
+++ Mi Cartelera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mi Cartelera
+//
+//  Created by Erinson Andres Romero Terry on 15/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
